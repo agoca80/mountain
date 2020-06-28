66a8a75fda67e050a78c396509e7be425514c132
Antxon <agoca80@gmail.com>
2020-06-28 15:03:36 +0200

    jenkins

--- filters/examplle.py
+++
@@ -1,9 +0,0 @@
-def format_list(list_, pattern):
-    return [pattern % s for s in list_]
-
-
-class FilterModule(object):
-    def filters(self):
-        return {
-            'format_list': format_list,
-        }
