5553778f3498d20df9e4e062d7c9d46fbeb4f706
Antxon <antxon.gonzalez@clarivate.com>
2021-01-22 22:38:20 +0100

    wip

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,12 +2,12 @@
 
 freebsd () {
     export ASSUME_ALWAYS_YES
-    
+
     password='$6$uxl5IBBDuGtqvmB6$xB3Qyjd72hrq8Q2LiWdcBjP1oe3DyFBQ2IO3k7jlg2lIbocv7q1hh7GnCjCB5I6tAj03mvv87/PikEMsznxr40'
     packages='python3 sudo'
 
     echo "$password" | pw mod user root -H 0
-    ASSUME_ALWAYS_YES="" pkg bootstrap 
+    pkg bootstrap 
     pkg install $packages
 }
 
