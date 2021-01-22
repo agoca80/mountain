655aea5bdc229f2ddecb4514bab471ede1b4df3a
Antxon <antxon.gonzalez@clarivate.com>
2021-01-22 22:30:59 +0100

    wip

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
-read -p "root password: " password
-echo "$password" | pw mod user root -h 0
+set -x
 
+freebsd () {
+    password='$6$uxl5IBBDuGtqvmB6$xB3Qyjd72hrq8Q2LiWdcBjP1oe3DyFBQ2IO3k7jlg2lIbocv7q1hh7GnCjCB5I6tAj03mvv87/PikEMsznxr40'
+    packages='python3 sudo'
+
+    echo "$password" | pw mod user root -H 0
+    pkg install $packages
+}
+
+freebsd
