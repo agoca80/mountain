733a9d93fab86e9863e1a76c53fd0966b813acb7
Antxon <antxon.gonzalez@clarivate.com>
2021-01-22 22:51:31 +0100

    wip

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 freebsd () {
-    export ASSUME_ALWAYS_YES
+    export ASSUME_ALWAYS_YES="yes"
 
     password='$6$uxl5IBBDuGtqvmB6$xB3Qyjd72hrq8Q2LiWdcBjP1oe3DyFBQ2IO3k7jlg2lIbocv7q1hh7GnCjCB5I6tAj03mvv87/PikEMsznxr40'
     packages='python3 sudo'
@@ -10,5 +10,7 @@ freebsd () {
     pkg bootstrap 
     pkg install $packages
 }
+
+set -x
 
 freebsd
