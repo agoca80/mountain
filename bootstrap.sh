ba45b34517a06da2a92efe57b2c99021cb7c0ab5
Antxon <antxon.gonzalez@clarivate.com>
2021-01-22 23:17:13 +0100

    wip

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 password='$6$uxl5IBBDuGtqvmB6$xB3Qyjd72hrq8Q2LiWdcBjP1oe3DyFBQ2IO3k7jlg2lIbocv7q1hh7GnCjCB5I6tAj03mvv87/PikEMsznxr40'
-
 packages='python3 sudo'
 
 freebsd () {
@@ -9,6 +8,7 @@ freebsd () {
 
     echo "$password" | pw mod user root  -H 0 
     echo "$password" | pw add user admin -H 0 -mn -G wheel
+
     pkg bootstrap 
     pkg install $packages
 }
